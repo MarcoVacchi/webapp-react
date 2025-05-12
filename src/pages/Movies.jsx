771bf5da7f29bf42ae3cf00d16a3811e8343c0dc
MarcoVacchi <mvacchi816@gmail.com>
2025-05-12 14:27:31 +0200

    feat: finish exercise and added bonus search

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -5,10 +5,16 @@ import axios from "axios";
 export default function Movies() {
 
     const [movies, setMovies] = useState([]);
-    const endPoint = ('http://127.0.0.1:3000/movies')
+    const [search, setSearch] = useState('');
+
+    const endPoint = ('http://127.0.0.1:3000/movies');
 
     function getMovies() {
-        axios.get(endPoint)
+        axios.get(endPoint, {
+            params: {
+                search
+            }
+        })
             .then(res => {
                 setMovies(res.data);
                 console.log(res.data)
@@ -18,10 +24,28 @@ export default function Movies() {
 
     useEffect(getMovies, []);
 
+    function searchMovies(event) {
+        event.preventDefault();
+        getMovies();
+    }
+
     return <div>
         <h1>Movie</h1>
         <section>
             <h2>Best movie</h2>
+
+            <form onSubmit={searchMovies} className="row g-1">
+
+                <div className="col-auto">
+                    <input type="text" className="form-control" id="inputPassword2" placeholder="Cerca film"
+                        value={search} onChange={(e) => setSearch(e.target.value)}
+                    />
+
+                </div>
+                <div className="col-auto">
+                    <button type="submit" className="btn btn-primary mb-3">Search film</button>
+                </div>
+            </form>
 
             <div className="row g-3 container d-flex my-cont">
                 {movies.length ? (
