d4148a071f637044f402034da403366c8b6dccc9
MarcoVacchi <mvacchi816@gmail.com>
2025-05-16 01:17:57 +0200

    feat: fixed css

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,24 +1,17 @@
 import { useState } from "react";
 import axios from "axios";
+import { useNavigate } from 'react-router-dom';
 
-export default function AddMovie({ onSubmitSuccess }) {
-    const initiatValues = {
+export default function AddMovie() {
+    const [formData, setFormData] = useState({
         title: '',
         director: '',
         genre: '',
         abstract: '',
         image: null
-    };
-    const [formData, setFormData] = useState(initiatValues);
+    });
 
-    function setField(e) {
-        const value = e.target.type === 'file' ? e.target.files[0] : e.target.value;
-        setFormData((prevFormData) => ({
-            ...prevFormData,
-            [e.target.name]: value
-        }));
-    }
-
+    const navigate = useNavigate();
 
     const sendForm = (e) => {
         e.preventDefault();
@@ -26,7 +19,7 @@ export default function AddMovie({ onSubmitSuccess }) {
         if (!formData.image) {
             alert("Per favore, carica un'immagine.");
             return;
-        };
+        }
 
         axios.post("http://127.0.0.1:3000/movies", formData, {
             headers: {
@@ -34,31 +27,34 @@ export default function AddMovie({ onSubmitSuccess }) {
             }
         })
             .then(res => {
-                setFormData(initiatValues)
-
-                if (typeof onSubmitSuccess === "function") {
-                    onSubmitSuccess();
-                }
+                setFormData({
+                    title: '',
+                    director: '',
+                    genre: '',
+                    abstract: '',
+                    image: null
+                });
+                navigate("/movies");
             })
-
             .catch(err => {
                 console.error("Errore nel salvataggio:", err);
             });
     };
 
-
-    return <main className="bg-dark add-height text-white">
+    return <div className="bg-dark text-white add-heigth">
         <div className="container">
-            <form onSubmit={sendForm} className="container-sm">
+            <form className="mb-3" onSubmit={sendForm}>
+                <h1 className="text-center mb-4">Aggiungi un Film</h1>
+
                 <div className="mb-3">
-                    <label htmlFor="title" className="form-label mt-3">Titolo</label>
+                    <label htmlFor="title" className="form-label">Titolo</label>
                     <input
                         type="text"
                         className="form-control"
                         id="title"
                         name="title"
                         value={formData.title}
-                        onChange={setField}
+                        onChange={(e) => setFormData({ ...formData, title: e.target.value })}
                     />
                 </div>
 
@@ -70,7 +66,7 @@ export default function AddMovie({ onSubmitSuccess }) {
                         id="director"
                         name="director"
                         value={formData.director}
-                        onChange={setField}
+                        onChange={(e) => setFormData({ ...formData, director: e.target.value })}
                     />
                 </div>
 
@@ -82,7 +78,7 @@ export default function AddMovie({ onSubmitSuccess }) {
                         id="genre"
                         name="genre"
                         value={formData.genre}
-                        onChange={setField}
+                        onChange={(e) => setFormData({ ...formData, genre: e.target.value })}
                     />
                 </div>
 
@@ -94,7 +90,7 @@ export default function AddMovie({ onSubmitSuccess }) {
                         name="abstract"
                         rows="3"
                         value={formData.abstract}
-                        onChange={setField}
+                        onChange={(e) => setFormData({ ...formData, abstract: e.target.value })}
                     ></textarea>
                 </div>
 
@@ -105,15 +101,17 @@ export default function AddMovie({ onSubmitSuccess }) {
                         className="form-control"
                         id="image"
                         name="image"
-                        onChange={setField}
+                        onChange={(e) => setFormData({ ...formData, image: e.target.files[0] })}
                         accept="image/*"
                     />
                 </div>
 
                 <div className="d-flex justify-content-center">
-                    <button type="submit" className="btn btn-primary p-2 mb-3">Aggiungi Film</button>
+                    <button type="submit" className="btn btn-primary p-2 mb-3">
+                        Aggiungi Film
+                    </button>
                 </div>
             </form>
         </div>
-    </main>
+    </div>
 };
